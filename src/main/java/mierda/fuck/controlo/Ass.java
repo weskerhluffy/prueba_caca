103b1ab675bc800b224dec912371ed14a31f8527
ernesto.alvarado@oracle.com <ealvarad@slc10zbr.us.oracle.com>
2018-04-05 15:15:21 -0700

    chale

--- src/main/java/mierda/fuck/controlo/Ass.java
+++ src/main/java/mierda/fuck/controlo/Ass.java
@@ -2,11 +2,15 @@ package mierda.fuck.controlo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class Ass {
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping(value="/",produces="text/html")
+	public String indexxx(HttpServletResponse response) {
+		    response.setContentType("text/html");
+		    response.setCharacterEncoding("UTF-8");
 		return "index";
 	}
 }
